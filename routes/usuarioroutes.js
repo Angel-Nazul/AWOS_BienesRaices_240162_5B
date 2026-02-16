91384faf7b949aed11c4de7c78ae34bdf3fca59a
Angel-Nazul <240162@utxicotepec.edu.mx>
2026-02-16 16:12:34 -0600

    PC-BienesRaices - Seccion 03
    add: Se agrego la carpeta layout y un archivo index(pug), lo cual se puede acceder a login y registro junto con sus estilos

--- routes/usuarioroutes.js
+++ routes/usuarioroutes.js
@@ -1,13 +1,12 @@
 import express from 'express'
+import { formularioLogin, formularioRegistro} from '../controllers/usuarioController.js'
 
 const router = express.Router();
 
 // Definir los ENDPOINTS
 // GET
-router.get("/login", (req, res)=>{
-    
-    res.render("auth/login")
-})
+router.get("/login", formularioLogin)
+router.get("/registro", formularioRegistro)
 
 
 //POST
